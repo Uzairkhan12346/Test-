21a178644d92db0cb1f792a932c6db301e730ea2
Uzairkhan12346 <tempmailuzair@gmail.com>
2025-06-08 22:32:06 +0500

    Update 0-COUNT.js

--- models/commands/0-COUNT.js
+++ models/commands/0-COUNT.js
@@ -45,7 +45,7 @@ async function makeImage({ one, two }) {
 
   let circleOne = await jimp.read(await circle(avatarOne));
   let circleTwo = await jimp.read(await circle(avatarTwo));
-  batgiam_img.composite(circleOne.resize(284, 284), 68, 49).composite(circleTwo.resize(282, 282), 375, 45);
+  batgiam_img.composite(circleOne.resize(284, 284), 68, 49).composite(circleTwo.resize(282, 282), 375, 43);
 
   let raw = await batgiam_img.getBufferAsync("image/png");
 
