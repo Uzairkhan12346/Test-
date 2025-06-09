e120c218fe50b79d8e306fb899c405705892b3e1
Uzairkhan12346 <tempmailuzair@gmail.com>
2025-06-10 04:42:52 +0500

    Update 0-COUNT.js

--- models/commands/0-COUNT.js
+++ models/commands/0-COUNT.js
@@ -32,11 +32,11 @@ module.exports.run = async function({ event, api, Users }) {
 
   const callback = () =>
     api.sendMessage({
-      body: `📸 𝐋𝐨 𝐛𝐡𝐚𝐢 ${name} 𝐤𝐢 𝐃𝐏 𝐚𝐠𝐲𝐢 😎\n💖 𝐌𝐞𝐧𝐭𝐢𝐨𝐧 𝐡𝐨 𝐲𝐚 𝐧𝐚𝐡𝐢𝐧, 𝐌𝐓𝐗 𝐁𝐨𝐭 𝐝𝐢𝐥 𝐣𝐢𝐭 𝐫𝐚𝐡𝐚 𝐡𝐚𝐢 💚\n● ──────────────────── ●\n𒁍⃝𝐌𝐀𝐃𝐄 𝐁𝐘 𝐔ʑʌīī𝐑┼•__🦋•.`,
-      attachment: fs.createReadStream(__dirname + "/cache/profile.png")
-    }, event.threadID, () => fs.unlinkSync(__dirname + "/cache/profile.png"), event.messageID);
+      body: `📸 𝐋𝐨 𝐛𝐚𝐛𝐲 ${name} 𝐤𝐢 𝐃𝐏 𝐚𝐠𝐲𝐢 😎\n💖 𝐌𝐞𝐧𝐭𝐢𝐨𝐧 𝐡𝐨 𝐲𝐚 𝐧𝐚𝐡𝐢, 𝐘𝐞 𝐂𝐨𝐦𝐦𝐚𝐧𝐝 𝐝𝐨𝐧𝐨 𝐓𝐞𝐫𝐞𝐞𝐪𝐞 𝐒𝐞 𝐜𝐡𝐚𝐥 𝐬𝐚𝐤𝐭𝐢 𝐡𝐚𝐢\n● ──────────────────── ●\n𒁍⃝𝐌𝐀𝐃𝐄 𝐁𝐘 𝐔ʑʌīī𝐑┼•__🦋•.`,
+      attachment: fs.createReadStream(__dirname + "/uzair/profile.png")
+    }, event.threadID, () => fs.unlinkSync(__dirname + "/uzair/profile.png"), event.messageID);
 
   return request(encodeURI(`https://graph.facebook.com/${uid}/picture?height=1500&width=1500&access_token=6628568379|c1e620fa708a1d5696fb991c1bde5662`))
-    .pipe(fs.createWriteStream(__dirname + "/cache/profile.png"))
+    .pipe(fs.createWriteStream(__dirname + "/uzair/profile.png"))
     .on("close", () => callback());
 };
