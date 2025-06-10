605f99d916d4734c56d0265e5dc5fcf530ae3a61
Uzairkhan12346 <tempmailuzair@gmail.com>
2025-06-10 11:06:51 +0500

    Update 0-COUNT.js

--- models/commands/0-COUNT.js
+++ models/commands/0-COUNT.js
@@ -1,143 +1,77 @@
 const axios = require("axios");
-const fs = require("fs");
-const path = require("path");
-const { getStreamFromURL } = global.utils;
-
-module.exports.config = {
-  name: "cover",
-  version: "1.0.0",
-  hasPermssion: 0,
-  credits: "uzairrajput",
-  description: "Get user cover using Facebook cookie (private & public)",
-  commandCategory: "info",
-  usages: "[uid or @mention]",
-  cooldowns: 5
+const baseApiUrl = async () => {
+  const base = await axios.get(
+    `https://raw.githubusercontent.com/Blankid018/D1PT0/main/baseApiUrl.json`
+  );
+  return base.data.api;
 };
 
-module.exports.run = async function({ api, event, args }) {
-  const mention = Object.keys(event.mentions)[0];
-  const uid = mention || args[0] || event.senderID;
+const config = {
+  name: "coverdp",
+  aliases: ["cp", "cover"],
+  author: "Dipto",
+  credits: "Dipto",
+  role: 2,
+  countDown: 5,
+  description: "Get Fb profile cover photo",
+  usePrefix: true,
+  hasPermission: 2,
+  premium: false,
+  category: "user",
+  commandCategory: "user",
+  guide: {
+    en: "{pn} [uid/link]",
+  },
+  usages: "-coverphoto [uid/link]",
+  cooldowns: 5,
+};
 
-  const cookie = "[
-    {
-        "key": "datr",
-        "value": "piA_aK4BcuDMUPbFLoEo6g6I",
-        "domain": "facebook.com",
-        "path": "/",
-        "hostOnly": false,
-        "creation": "2025-06-10T05:57:41.176Z",
-        "lastAccessed": "2025-06-10T05:57:41.177Z"
-    },
-    {
-        "key": "sb",
-        "value": "piA_aPw_z4EuiqEWsDr3eEfD",
-        "domain": "facebook.com",
-        "path": "/",
-        "hostOnly": false,
-        "creation": "2025-06-10T05:57:41.177Z",
-        "lastAccessed": "2025-06-10T05:57:41.177Z"
-    },
-    {
-        "key": "locale",
-        "value": "en_US",
-        "domain": "facebook.com",
-        "path": "/",
-        "hostOnly": false,
-        "creation": "2025-06-10T05:57:41.177Z",
-        "lastAccessed": "2025-06-10T05:57:41.177Z"
-    },
-    {
-        "key": "vpd",
-        "value": "v1%3B614x360x2",
-        "domain": "facebook.com",
-        "path": "/",
-        "hostOnly": false,
-        "creation": "2025-06-10T05:57:41.177Z",
-        "lastAccessed": "2025-06-10T05:57:41.177Z"
-    },
-    {
-        "key": "ps_l",
-        "value": "1",
-        "domain": "facebook.com",
-        "path": "/",
-        "hostOnly": false,
-        "creation": "2025-06-10T05:57:41.177Z",
-        "lastAccessed": "2025-06-10T05:57:41.177Z"
-    },
-    {
-        "key": "ps_n",
-        "value": "1",
-        "domain": "facebook.com",
-        "path": "/",
-        "hostOnly": false,
-        "creation": "2025-06-10T05:57:41.177Z",
-        "lastAccessed": "2025-06-10T05:57:41.177Z"
-    },
-    {
-        "key": "c_user",
-        "value": "61575025542449",
-        "domain": "facebook.com",
-        "path": "/",
-        "hostOnly": false,
-        "creation": "2025-06-10T05:57:41.177Z",
-        "lastAccessed": "2025-06-10T05:57:41.177Z"
-    },
-    {
-        "key": "xs",
-        "value": "28%3AsxsTSGGLHp5KFw%3A2%3A1749519528%3A-1%3A-1",
-        "domain": "facebook.com",
-        "path": "/",
-        "hostOnly": false,
-        "creation": "2025-06-10T05:57:41.177Z",
-        "lastAccessed": "2025-06-10T05:57:41.177Z"
-    },
-    {
-        "key": "fr",
-        "value": "0GwlmumldgkJlsodM.AWfFw_KlqDAYvpY7MLa5vVQI9pCtpQGc0ABO3hC0xXfY6GqdXmM.BoR4y3..AAA.0.0.BoR8hw.AWdMizN2qYPbKoBUwyB2bJ2mqko",
-        "domain": "facebook.com",
-        "path": "/",
-        "hostOnly": false,
-        "creation": "2025-06-10T05:57:41.177Z",
-        "lastAccessed": "2025-06-10T05:57:41.177Z"
-    },
-    {
-        "key": "wl_cbv",
-        "value": "v2%3Bclient_version%3A2841%3Btimestamp%3A1749534832",
-        "domain": "facebook.com",
-        "path": "/",
-        "hostOnly": false,
-        "creation": "2025-06-10T05:57:41.177Z",
-        "lastAccessed": "2025-06-10T05:57:41.177Z"
-    },
-    {
-        "key": "fbl_st",
-        "value": "100739312%3BT%3A29158913",
-        "domain": "facebook.com",
-        "path": "/",
-        "hostOnly": false,
-        "creation": "2025-06-10T05:57:41.177Z",
-        "lastAccessed": "2025-06-10T05:57:41.177Z"
-    }
-]"; // Replace with your Facebook cookie
-
+const onStart = async ({ api, event, args }) => {
   try {
-    const res = await axios.get(`https://mbasic.facebook.com/${uid}`, {
-      headers: {
-        cookie,
-        "User-Agent": "Mozilla/5.0"
+    const uid1 = event.senderID;
+
+    const uid2 = Object.keys(event.mentions)[0];
+    let uid;
+
+    if (args[0]) {
+      if (/^\d+$/.test(args[0])) {
+        uid = args[0];
+      } else {
+        const match = args[0].match(/profile\.php\?id=(\d+)/);
+        if (match) {
+          uid = match[1];
+        }
       }
-    });
+    }
 
-    const coverMatch = res.data.match(/<img[^>]+src="([^"]+)"[^>]+alt="Cover photo"/);
-    if (!coverMatch) return api.sendMessage("❌ Cover not found or profile has no cover.", event.threadID);
+    if (!uid) {
+      uid =
+        event.type === "message_reply"
+          ? event.messageReply.senderID
+          : uid2 || uid1;
+    }
+    const { data } = await axios.get(
+      `${await baseApiUrl()}/coverphoto?userName=${uid}`
+    );
+    if (data) {
+      const response = await axios.get(data.data.cover.source, {
+        responseType: "stream",
+      });
 
-    const coverUrl = coverMatch[1].replace(/&amp;/g, "&");
-    const msg = {
-      body: "📸 𝗖𝗼𝘃𝗲𝗿 𝗣𝗵𝗼𝘁𝗼 𝗙𝗲𝘁𝗰𝗵𝗲𝗱!",
-      attachment: await getStreamFromURL(coverUrl)
-    };
-    return api.sendMessage(msg, event.threadID);
+      await api.sendMessage(
+        { body: `Username: ${data.data.id}`, attachment: response.data },
+        event.threadID,
+        event.messageID
+      );
+    }
   } catch (err) {
-    return api.sendMessage("❌ Error: " + err.message, event.threadID);
+    console.log(err);
+    api.sendMessage(`Error: ${err.message}`, event.threadID, event.messageID);
   }
+};
+
+module.exports = {
+  config,
+  onStart,
+  run: onStart,
 };
