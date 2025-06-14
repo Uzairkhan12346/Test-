6b536be84f07a85b5d3385e29238c4679aba37c7
Uzairkhan12346 <tempmailuzair@gmail.com>
2025-06-15 01:18:32 +0500

    Update 0-FRIENDS.js

--- models/commands/0-FRIENDS.js
+++ models/commands/0-FRIENDS.js
@@ -52,8 +52,8 @@ async function makeImage({ one, two, three }) {
 
   // Resize and place on frame
   frame.composite(circle2.resize(149, 149), 30, 142);   // Left friend
-  frame.composite(circle1.resize(152, 152), 240, 29);  // Center (you)
-  frame.composite(circle3.resize(149, 149), 480, 142);  // Right friend
+  frame.composite(circle1.resize(152, 152), 240, 28);  // Center (you)
+  frame.composite(circle3.resize(145, 145), 480, 142);  // Right friend
 
   // Save final image
   let buffer = await frame.getBufferAsync("image/png");
